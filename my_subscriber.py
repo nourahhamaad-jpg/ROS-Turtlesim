ad5a02c9b3f7cd67a65ce9fabb9d91b4cc306471
Nourah <nourah.hamaad3967@gmail.com>
2026-08-06 01:07:15 +0300

    Create my_subscriber.py

---
+++ my_subscriber.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import String
+
+def callback(data):
+    rospy.loginfo("I heard: %s", data.data)
+
+def listener():
+    rospy.init_node('nourah_subscriber', anonymous=True)
+    rospy.Subscriber('custom_chatter', String, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
